985a80b6013e2fdc1903700a2ba4c7e0022ff4f4
Sergio <scm0@correo.ugr.es>
2019-02-28 00:40:09 +0100

    main modificado

--- practica2/src/main.cpp
+++ practica2/src/main.cpp
@@ -7,7 +7,7 @@
 
    Un ejemplo de ejecución es:
 
-    Cuantos intervalos (max 10):2 
+    Cuantos intervalos (max 10):2
     Introduce [ o ( cotaInferior, cotaSuperior ) o ]: [0,10] (0,10]
     Da como salida:  (0,10]
 
@@ -21,18 +21,39 @@ using namespace std;
 
 
 int main(){
+/*
     const int CAPACIDAD = 10;
     int i, nintervalos=0;
-    
+
     cout << "Cuantos intervalos (max 10):";
-...
+
+    cin >> nintervalos;
 
     cout << "Introduce [ o ( cotaInferior, cotaSuperior ) o ]";
     for(i=0; i < nintervalos; i++)
-        leer(...);
-    
+        leer();
+
 ...
     escribir(resultado);
     }
-    return 0;   
+*/
+    Intervalo i1, i2;
+/*
+    cout << "Introduce 2 intervalos:" << endl;
+    cout << "\tIntroduce el primer intervalo: ";
+    leer(i1);
+    cout << "\tIntroduce el segundo intervalo: ";
+    leer(i2);
+*/
+    int n;
+    cin >> n;
+
+    leer (i1);
+    leer (i2);
+
+    Intervalo resultado = interseccion (i1, i2);
+
+    escribir (resultado);
+    cout << endl;
+    return 0;
 }
