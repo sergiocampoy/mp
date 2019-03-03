39da650ff42bad5c381a08a941c39c26e15718f0
Sergio <scm0@correo.ugr.es>
2019-03-03 11:44:20 +0100

    Borrada la documentación de Deoxys

--- practica1/doc/html/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['estadentro',['estaDentro',['../classIntervalo.html#a2cccd9264f1b3912c6006fe3e2a70289',1,'Intervalo']]],
-  ['esvacio',['esVacio',['../classIntervalo.html#adc77e18147f9f9f85476a0d44257bb02',1,'Intervalo']]]
-];
