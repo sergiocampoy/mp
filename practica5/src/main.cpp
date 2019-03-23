3c90a6e7225c724575b47eefe8b8ca5ca783a590
Sergio <scm0@correo.ugr.es>
2019-03-23 11:24:26 +0100

    Corregido main

--- practica5/src/main.cpp
+++ practica5/src/main.cpp
@@ -3,31 +3,31 @@
 using namespace std;
 
 int main (){
-    
+
     const int TAM = 100;
-    
+
     // Se declarala cadena base
     char base[] = "cadena base para prueba de insercion";
     // Se declarala cadena que se inserta
     char aInsertar[] = "−agregado−";
-    
+
     char resultado[TAM];
-    
+
     int pos;
-    
+
     cin >> pos;
-    
+
     bool proceso;
-    
-    proceso = insertarCadena(base[], aInsertar[], pos, resultado[]);
-    
+
+    proceso = insertarCadena(base, aInsertar, pos, resultado);
+
     if(proceso){
-        for(int i=0; resultado[i] =! 0; i++){
+        for(int i=0; resultado[i] != 0; i++){
             cout << resultado[i];
         }
     }
     else
         cout << "La insercion no ha sido posible";
-    
+
     return 0;
 }
