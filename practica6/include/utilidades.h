8e73470b642eaa1b09e11a6f00dc44005b6ac27c
Sergio <scm0@correo.ugr.es>
2019-03-26 20:14:00 +0100

    Añadida práctica 6

---
+++ practica6/include/utilidades.h
@@ -0,0 +1,46 @@
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+#include <iostream>
+
+using namespace std;
+
+
+const int CAPACIDAD = 20;
+
+/**
+ * @brief lee un vecter de enteros, primero el numero de elementos seguido de las entradas
+ * @param coleccion vector de enteros
+ * @param util numero de componentes de coleccion
+ */
+void leerContenido(int coleccion[], int & util);
+
+/**
+ * @brief muestra en pantalla el contenido de vector, primero numero de componentes seguido de los valores
+ * @param array vector de enteros
+ * @param utilArray numero de componentes del vector
+ */
+
+void imprimirArray(const int array[], int utilArray);
+
+/**
+ * @brief devuelve un subvector con la mayor secuencia monotona creciente
+ * @param array, vector original de entrada
+ * @param utilArray numero de componentes del vector origianl
+ * @param secuenciaCreciente vector de salida
+ * @param utilSecuenciaCreciente numero de componentes del vector de salida
+ */
+void obtenerMayorSecuenciaMonotonaCreciente(const int *array, int utilArray,
+                                            const int *&secuenciaCreciente, int &utilSecuenciaCreciente);
+
+
+/**
+ * @brief halla el minimo y el máximo valor de un vector
+ * @param array ector original de entrada
+ * @param utilArray  numero de componentes del vector original
+ * @param *pmin puntero al minimo valor
+ * @param *pmax  puntero al máximo valor
+ */
+void hallarMinMax(const int *array, int utilArray,
+                                            const int *&pmin, const int *&pmax);
+
+#endif //UTILIDADES_H
---
+++ practica6/src/utilidades.cpp
@@ -0,0 +1,84 @@
+
+#include "utilidades.h"
+using namespace std;
+
+
+void leerContenido(int *coleccion, int & util){
+    do{
+//      cout << "Introduce cuantos: ";
+      cin >> util;
+    }while (util > CAPACIDAD);
+
+    for(int i = 0; i < util; i++){
+        cin >> coleccion[i];
+    }
+}
+
+/**
+ * @brief Muestra los elementos del array en la salida estandar
+ * @param array array a imprimir
+ * @param utilArray numero de elementos utiles en el array
+ * @pre array debería contener al menos un elemento
+ */
+void imprimirArray(const int *array, int utilArray) {
+    cout << utilArray << ' ';
+    for (int i = 0; i < utilArray; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+/**
+  * @brief Obtiene un array con la secuencia más larga creciente de números enteros
+  * @param array puntero a primer componente del array de entrada
+  * @param utilArray numero de elementos utiles en el array
+  * @param secuenciaCreciente puntero a primer componente de salida con la secuencia más larga creciente
+  * @param util_secuenciaCreciente numero de elementos utiles en secuenciaCreciente
+ */
+
+void obtenerMayorSecuenciaMonotonaCreciente(const int *array, int utilArray, const int *&secuenciaCreciente, int &utilSecuenciaCreciente) {
+    const int *p;   // Apuntador
+
+    const int *posRacha;    // Pos inicio de la cadena con mejor racha temp
+    posRacha = array;
+    int utilRacha = 1;       // Longitud de la cadena con mejor racha temp
+
+    secuenciaCreciente = array; // Pos inicio de la cadena con mejor racha
+    utilSecuenciaCreciente = 1; // Longitud de la cadena con mejor racha
+
+
+    for (p = array + 1; p < array + utilArray; p++) {
+        if (*p > *(p-1)) {
+            utilRacha++;
+        }
+        else {
+            if (utilRacha > utilSecuenciaCreciente) {
+                utilSecuenciaCreciente = utilRacha;
+                secuenciaCreciente = posRacha;
+            }
+            utilRacha = 1;
+            posRacha = p;
+        }
+    }
+
+    if (utilRacha > utilSecuenciaCreciente) {
+        utilSecuenciaCreciente = utilRacha;
+        secuenciaCreciente = posRacha;
+    }
+}
+
+
+void hallarMinMax(const int *array, int utilArray, const int *&pmin, const int *&pmax){
+    const int *p;
+
+    pmin = array;
+    pmax = array;
+
+    for (p = array; p < array + utilArray; p++) {
+        if (*p < *pmin)
+            pmin = p;
+
+        else if (*p > *pmax)
+            pmax = p;
+    }
+}
